a06120ccf876431c967af4ef7ccd3ea64558bcf1
Jesus <97321002+jesusjson@users.noreply.github.com>
2022-04-02 21:55:05 +0300

    Add files via upload

---
+++ phppdoveriguncelleme.php
@@ -0,0 +1,22 @@
+<?php 
+// VT Baglantı dosyasını include ediyoruz.
+include 'baglan.php';
+
+
+// Kayit Butonuna Basılma Durumunda
+if(isset($_POST['güncelle'])){
+    $eskideger = $_POST['eskiveri'];
+    $yenideger = $_POST['yeniveri'];
+
+    $güncelle = $db -> prepare("UPDATE users SET username = ? WHERE username = ?");
+    $güncelle -> execute([
+        $yenideger, $eskideger
+    ]);
+}
+
+
+
+
+
+
+?>
